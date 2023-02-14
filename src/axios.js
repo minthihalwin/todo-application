8ab512709911053ff716686fc3ec9874ad77f414
minthihalwin <minthihalwin.dev@gmail.com>
2023-02-15 11:43:36 +1300

    Finalized project and hosted in firebase and ready to fire in air! cheer

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://us-central1-todo-app-api-1bd67.cloudfunctions.net/api",
 });
 
 export default instance;
